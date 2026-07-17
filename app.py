c2cb1bb2885abab359c30603e03e5b8efcb3c5c2
rakeshgoudsilveru27 <rakeshgoudsilveru27@gmail.com>
2026-07-17 15:23:29 +0530

    updated srg glasses server again

--- app.py
+++ app.py
@@ -802,3 +802,6 @@ User:
         "heard": user_message,
         "reply": ai_reply
     })
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
